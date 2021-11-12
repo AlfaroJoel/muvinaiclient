987feab882a0629718e07e7f789aa811c6ebe11c
Joel Alfaro <alfarojoelf@gmail.com>
2021-11-12 03:46:37 -0300

    add history edits

---
+++ src/Components/Sections/HistoryEdits.js
@@ -0,0 +1,102 @@
+import React from 'react';
+import Section from './Section';
+import Paper from '@mui/material/Paper';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TablePagination from '@mui/material/TablePagination';
+import TableRow from '@mui/material/TableRow';
+import { useSelector } from 'react-redux';
+
+const columns = [
+  { id: 'user', label: 'Usuario'},
+  { id: 'name', label: 'Nombre'},
+  { id: 'action', label: 'Accion'},
+  { id: 'from', label: 'De'},
+  { id: 'to', label: 'A'}
+];
+
+const HistoryEdits = () => {
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+  const client = useSelector(state => state.client.item);
+  const rows = client.historyEdit;
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
+  return (
+    <Section title='Historial de Edicion' minHeight='470px'>
+      <Paper sx={{ width: '100%', minHeight: '380px', overflow: 'hidden' }}>
+        <TableContainer 
+          sx={{ maxHeight: '330px', minHeight:'330px',
+            '::-webkit-scrollbar': {
+              width: '.3em',
+            },
+            '::-webkit-scrollbar-track': {
+              '-webkit-box-shadow': 'inset 0 0 6px rgba(0,0,0,0.00)',
+            },
+            '::-webkit-scrollbar-thumb': {
+              backgroundColor: 'rgba(0,0,0,.1)',
+              borderRadius: '10px'
+            }}
+          }>
+          <Table stickyHeader aria-label="sticky table">
+            <TableHead variant='outlined'>
+              <TableRow>
+                {columns.map((column) => (
+                  <TableCell
+                    key={column.id}
+                    align={column.align}
+                    style={{ minWidth: column.minWidth }}
+                  >
+                    {column.label}
+                  </TableCell>
+                ))}
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rows
+                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                .map((row) => {
+                  return (
+                    <TableRow key={row.id} hover role="checkbox" tabIndex={-1}>
+                      {columns.map((column) => {
+                        const value = row[column.id];
+                        return (
+                          <TableCell key={column.id} align={column.align}>
+                            {column.format && typeof value === 'number'
+                              ? column.format(value)
+                              : value}
+                          </TableCell>
+                        );
+                      })}
+                    </TableRow>
+                  );
+                })}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        <TablePagination
+          rowsPerPageOptions={[5, 10, 15]}
+          component="div"
+          count={rows.length}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          sx={{position:'relative', bottom:'0'}}
+          onPageChange={handleChangePage}
+          onRowsPerPageChange={handleChangeRowsPerPage}
+        />
+      </Paper>
+    </Section>
+  );
+};
+
+export default HistoryEdits;
